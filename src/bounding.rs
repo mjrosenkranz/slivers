5a23ef66c00f9517f462c81c3badee28ca1afe0a
Morgan Rosenkranz <rosenkranz1999@gmail.com>
2024-04-08 21:26:24 -0400

    implemented add and volume for aabb

--- src/bounding.rs
+++ src/bounding.rs
@@ -1,10 +1,12 @@
+use std::ops::Add;
+
 use nalgebra::Vector3;
 
 pub trait Bounds {
     fn get_bounds(&self) -> AABB;
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub struct AABB {
     min: Vector3<f32>,
     max: Vector3<f32>,
@@ -14,5 +16,57 @@ impl AABB {
     pub fn new(min: Vector3<f32>, max: Vector3<f32>) -> Self {
         Self { min, max }
     }
+
+    pub fn volume(self) -> f32 {
+        let mag = self.max - self.min;
+        mag.x * mag.y * mag.z
+    }
 }
 
+impl Add for AABB {
+    type Output = AABB;
+
+    fn add(self, rhs: AABB) -> AABB {
+        AABB::new(
+            Vector3::new(
+                f32::min(self.min.x, rhs.min.x),
+                f32::min(self.min.y, rhs.min.y),
+                f32::min(self.min.z, rhs.min.z),
+            ),
+            Vector3::new(
+                f32::max(self.max.x, rhs.max.x),
+                f32::max(self.max.y, rhs.max.y),
+                f32::max(self.max.z, rhs.max.z),
+            ),
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn init() {
+        let bb = AABB::new(Vector3::new(0., 0., 0.), Vector3::new(1., 1., 1.));
+        assert_eq!(bb.volume(), 1.);
+    }
+
+    #[test]
+    fn vol() {
+        let bb = AABB::new(Vector3::new(0., 0., -3.), Vector3::new(3., 4., -1.));
+        assert_eq!(bb.volume(), 24.);
+    }
+
+    #[test]
+    fn add_bb() {
+        let bb1 = AABB::new(Vector3::new(-1., -2., -3.), Vector3::new(0., 0., 0.));
+        let bb2 = AABB::new(Vector3::new(0., 0., 0.), Vector3::new(4., 5., 6.));
+        let bb3 = bb1 + bb2;
+
+        assert_eq!(
+            bb3,
+            AABB::new(Vector3::new(-1., -2., -3.), Vector3::new(4., 5., 6.))
+        );
+    }
+}
--- src/primitives.rs
+++ src/primitives.rs
@@ -1,10 +1,16 @@
+use crate::bounding::{Bounds, AABB};
 use nalgebra::Vector3;
-use crate::bounding_box::{AABB, Bounds};
 
 #[derive(Debug)]
 pub struct Sphere {
     pos: Vector3<f32>,
     radius: f32,
+}
+
+impl Sphere {
+    fn new(pos: Vector3<f32>, radius: f32) -> Self {
+        Self { pos, radius }
+    }
 }
 
 impl Bounds for Sphere {
@@ -18,18 +24,14 @@ impl Bounds for Sphere {
 mod tests {
     use super::*;
 
-
     #[test]
     fn sphere() {
-        let s = Sphere {
-            pos: Vector3::new(0., 3., -1.5),
-            radius: 1.0,
-        };
+        let s = Sphere::new(Vector3::new(0., 3., -1.5), 1.0);
         let b = s.get_bounds();
 
-        assert_eq!(b, AABB::new(
-            Vector3::new(-1.0, 2.0, -2.5),
-            Vector3::new(1.0, 4.0, -0.5),
-        ));
+        assert_eq!(
+            b,
+            AABB::new(Vector3::new(-1.0, 2.0, -2.5), Vector3::new(1.0, 4.0, -0.5),)
+        );
     }
 }
